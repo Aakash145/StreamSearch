1e5bac53a5e2967d6883438e6240298af9b79744
Aakash Saini <aakashsaini769@gmail.com>
2021-12-16 19:05:20 -0800

    Fixed Mobile View Issues

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,8 +4,8 @@ import './footer.css'
 function Footer() {
     return (
         <div className="row">
-        <footer id="sticky-footer" className="flex-shrink-0 py-4 container-fluid footer fixed-bottom">
-        <div className="container text-center">
+        <footer id="sticky-footer" className="flex-shrink-0 py-6 container-fluid footer fixed-bottom">
+        <div className="container text-center copyrightData">
         <small>Copyright &copy; Aakash Saini, Website: <a href="https://www.sainiaakash.com" >www.sainiaakash.com</a></small>
         </div>
         </footer>
